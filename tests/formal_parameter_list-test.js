72275881329d621bd04dbae2e4a017c3b16ed291
Nikita Chursin <nchursin@aquivalabs.com>
2018-09-28 23:46:37 -0700

    removed ... from tests due to Apex doesn't support it

--- tests/formal_parameter_list-test.js
+++ tests/formal_parameter_list-test.js
@@ -1,7 +1,5 @@
 const Parser = require('../src/index')
 
-const MismatchedTokenException = require('chevrotain').MismatchedTokenException
-
 describe('formalParameterList', () => {
   it('one formalParameter', () => {
     expect(Parser.parse('boolean a', (parser) => parser.formalParameterList())).toEqual([
@@ -62,52 +60,5 @@ describe('formalParameterList', () => {
         },
       },
     ])
-  })
-
-  it('last is dotDotDot', () => {
-    expect(
-      Parser.parse('boolean a, boolean... b', (parser) => parser.formalParameterList())
-    ).toEqual([
-      {
-        type: 'FORMAL_PARAMETER',
-        modifiers: [],
-        typeType: {
-          type: 'PRIMITIVE_TYPE',
-          value: 'boolean',
-        },
-        dotDotDot: false,
-        id: {
-          type: 'VARIABLE_DECLARATOR_ID',
-          id: {
-            type: 'IDENTIFIER',
-            value: 'a',
-          },
-          dimensions: [],
-        },
-      },
-      {
-        type: 'FORMAL_PARAMETER',
-        modifiers: [],
-        typeType: {
-          type: 'PRIMITIVE_TYPE',
-          value: 'boolean',
-        },
-        dotDotDot: true,
-        id: {
-          type: 'VARIABLE_DECLARATOR_ID',
-          id: {
-            type: 'IDENTIFIER',
-            value: 'b',
-          },
-          dimensions: [],
-        },
-      },
-    ])
-  })
-
-  it('not last is dotDotDot -> excpect error', () => {
-    expect(() =>
-      Parser.parse('boolean... a, boolean b', (parser) => parser.formalParameterList())
-    ).toThrowError(MismatchedTokenException)
   })
 })
