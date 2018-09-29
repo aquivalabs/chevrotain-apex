12f1dc32c244626de98a1aef9bf28a2623811a03
Nikita Chursin <nchursin@aquivalabs.com>
2018-09-29 00:07:31 -0700

    remove unsupported syntax from tests/annotation_method_rest-test.js

--- tests/annotation_method_rest-test.js
+++ tests/annotation_method_rest-test.js
@@ -11,30 +11,4 @@ describe('annotationMethodRest', () => {
       defaultValue: undefined,
     })
   })
-
-  it('defaultValue', () => {
-    expect(Parser.parse('a() default @Bean', (parser) => parser.annotationMethodRest())).toEqual({
-      type: 'ANNOTATION_METHOD_REST',
-      name: {
-        type: 'IDENTIFIER',
-        value: 'a',
-      },
-      defaultValue: {
-        type: 'DEFAULT_VALUE',
-        value: {
-          type: 'ANNOTATION',
-          name: {
-            type: 'QUALIFIED_NAME',
-            name: [
-              {
-                type: 'IDENTIFIER',
-                value: 'Bean',
-              },
-            ],
-          },
-          hasBraces: false,
-        },
-      },
-    })
-  })
 })
