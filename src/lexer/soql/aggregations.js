6a97fc916c0f19e3bac6887102083c538672119d
Dmitry Nikitin <dnikitin@aquivalabs.com>
2018-10-05 18:42:34 +0300

    added SOQL aggregation functions

--- src/lexer/soql/aggregations.js
+++ src/lexer/soql/aggregations.js
@@ -1,9 +1,39 @@
 const { createKeywordToken } = require('../_shared')
 
+const CountDistinct = createKeywordToken({
+  name: 'COUNT_DISTINCT',
+  pattern: /COUNT_DISTINCT/,
+  label: "'COUNT DISTINCT'",
+})
+
 const Count = createKeywordToken({
   name: 'COUNT',
   pattern: /COUNT/,
   label: "'COUNT'",
+})
+
+const Avg = createKeywordToken({
+  name: 'AVG',
+  pattern: /AVG/,
+  label: "'AVG'",
+})
+
+const Min = createKeywordToken({
+  name: 'MIN',
+  pattern: /MIN/,
+  label: "'MIN'",
+})
+
+const Max = createKeywordToken({
+  name: 'MAX',
+  pattern: /MAX/,
+  label: "'MAX'",
+})
+
+const Sum = createKeywordToken({
+  name: 'SUM',
+  pattern: /SUM/,
+  label: "'SUM'",
 })
 
 const Group = createKeywordToken({
@@ -12,7 +42,19 @@ const Group = createKeywordToken({
   label: "'GROUP'",
 })
 
+const Having = createKeywordToken({
+  name: 'HAVING',
+  pattern: /HAVING/,
+  label: "'HAVING'",
+})
+
 module.exports = {
+  CountDistinct,
   Count,
+  Avg,
+  Min,
+  Max,
+  Sum,
   Group,
+  Having,
 }
